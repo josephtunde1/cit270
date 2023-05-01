115e315f68c771b33bfc8225272b6a7600989d9a
“tunde <ogu18003@byui.edu>
2023-05-01 12:33:44 -0600

    mondayy

--- Login.sh
+++ Login.sh
@@ -2,4 +2,4 @@ echo "Logging in"
 
 curl -v -d "@login.json" POST -H "Content-Type:application/json" https://dev.stedi.me/login
 
-curl https://dev.stedi.me/validate/95fa12c4-5e2b-412d-8509-3eb3596ae8e3
+curl -v https://dev.stedi.me/validate/95fa12c4-5e2b-412d-8509-3eb3596ae8e3
