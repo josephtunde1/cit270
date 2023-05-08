cb16d9ad9be45cd7d4ee0063ece98a218c0fdba4
“tunde <ogu18003@byui.edu>
2023-05-08 13:24:29 -0600

    monday

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
 const express=require('express');
-const app='express'();
-app.listen(3000)
+const bodyparser = require('body-parser');
+
+const app = express();
+
+const port = 3000
+
+app.use(bodyParser.json()); //allow JSON (Javascript Object notation) requests
+
+app.listen(port,()=>{
+    console.log("listening on port: "+port);
+});
+
+app.get('/', (reg, res) =>{
+    // res.redirect(301,'https://google.com');
+    res.send("welcome to your node server");
+})
+
+app.post('/login',(req,res)=>{
+    const loginBody = req.body;
+    const userName = loginBody.userName;
+    res.send("Welcome "+userName);
+});
