3b014bf4a4b5c7b3c953ac01f8011297d081e968
“tunde <ogu18003@byui.edu>
2023-05-17 13:50:59 -0600

    wedmay17

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express=require('express');
 const bodyParser = require('body-parser');
 const Redis = require('redis');
+
+import { createhash } from node:crypto ;
+
 const app = express();
 const port = 3000
 const redisClient = Redis.createclient({url:"redis://127.0.0.1:6379"});
@@ -8,10 +11,7 @@ const redisClient = Redis.createclient({url:"redis://127.0.0.1:6379"});
 app.use(bodyParser.json()); //allow JSON (Javascript Object notation) requests
 
 app.listen(port, ()=>{
-    redisClient.connect();
-
-    
-    
+    redisClient.connect(); //the API server is trying to connect with Redis
     console.log("listening on port: "+port);
 });
 
@@ -23,12 +23,12 @@ app.get('/', (reg, res) =>{
 app.post('/login',async (req,res)=>{
     const loginBody = req.body;
     const userName = loginBody.userName;
-    const password = loginBody.password;
+    const password = loginBody.password; //we need to hash the password the user gave us
 
-    const redisPassword = await redisClient.hGet("users", userName);
-    console.log("password for " + userName + redisPassword);
+    const redisPassword = await redisClient.hGet("hashedpasswords", userName);
+    console.log("password for "+userName + redisPassword);
 
-    if (redisPassword != null && password=== redisPassword) {
+    if (redisPassword!=null && password===redisPassword) {
     //this happens if the password is correct
     res.send('Welcome, $(userName)!');
     } else {
