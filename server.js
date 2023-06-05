4fac52c47b6a486b4f9e813b8852e15d48ccfc03
“tunde <ogu18003@byui.edu>
2023-06-05 13:50:02 -0600

    monday5

--- server.js
+++ server.js
@@ -15,8 +15,9 @@ const redisClient = Redis.createClient({url:'redis://127.0.0.1:6379'});
 app.use(bodyParser.json()); //allow JSON (Javascript Object notation) requests
 
 https.createServer({
-    key: fs.readFileSync('server.key'),
-    cert: fs.readFileSync('server.cert')
+    key: fs.readFileSync('privkey1.pem'), //This is a private key
+    cert: fs.readFileSync('cert1.pem'),
+    chain: fs.readFileSync('fullchain1.pem') //This is a self-signed certification
   }, app).listen(3000, () => {
     console.log('Listening...')
   });
