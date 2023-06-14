f5563f9d118897fdc0137b2dba3f0b7b8c161d40
“tunde <ogu18003@byui.edu>
2023-06-14 11:30:11 -0600

    wednesday14

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ https.createServer({
     //cert: fs.readFileSync('server.cert'),
     key: fs.readFileSync('/etc/letsencrypt/archive/josephtunde.cit270.com/privkey1.pem'), //This is a private key
     cert: fs.readFileSync('/etc/letsencrypt/archive/josephtunde.cit270.com/cert1.pem'), //This is a signed certificate
-    chain: fs.readFileSync('/etc/letsencrypt/archive/josephtunde.cit270.com/fullchain1.pem') //This is the certificate chain
+    ca: fs.readFileSync('/etc/letsencrypt/archive/josephtunde.cit270.com/chain1.pem') //This is the certificate chain
   }, app).listen(443, () => {
     redisClient.connect(); 
     console.log('Listening...')
